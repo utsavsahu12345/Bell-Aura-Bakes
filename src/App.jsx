88ce1908561a5238d5f583ecf4997758f9c37f88
utsavsahu12345 <utsavsahu12345@gmail.com>
2026-08-11 12:52:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Bellaurabakes from './Bellaurabakes';
+
+const App = () => {
+  return (
+    <div>
+      <Bellaurabakes />
+    </div>
+  )
+}
+
+export default App
